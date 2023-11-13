27e6cf7bb0c8b69f04bbecf53f0a34c6a81dc5c1
sikorskayaX <sikorskaya02@gmail.com>
2023-11-14 01:21:26 +0300

    Add slideshow functionality and initialize active slide
    This commit adds a slideshow functionality by:
    
    1. Wrapping the existing slide logic in a function that takes the initial active slide number as a parameter
    
    2. Calling the slideshow function on page load, passing the number 3 to initialize the 4th slide as active
    
    3. Updating the index.html to remove the hardcoded active class from one of the slides.
    
    This initializes the slideshow and sets an active slide on page load.

--- marathon-urok-1/app.js
+++ marathon-urok-1/app.js
@@ -1,14 +1,21 @@
-﻿const slides = document.querySelectorAll('.slide')
+﻿function pligunSlides(num){
+    
+    const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides){
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+    for (const slide of slides){
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+
+    slides[num].classList.add('active')
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+pligunSlides(3);

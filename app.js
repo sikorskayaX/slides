91660435cab6b968b63c9aebdb83d5931c0d4c8f
Ksenia <106336275+sikorskayaX@users.noreply.github.com>
2025-08-27 00:04:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿function pligunSlides(num){
+    
+    const slides = document.querySelectorAll('.slide')
+
+    for (const slide of slides){
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+
+    slides[num].classList.add('active')
+}
+
+pligunSlides(3);
